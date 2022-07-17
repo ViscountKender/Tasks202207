7c04f56b94e576a141e3c4515693c130ef725418
Viscount <siyanov.d@sar.garm.ru>
2022-07-17 23:50:46 +0400

    commit

---
+++ Tasks/src/main/java/ru/viscount/app/Calculator.java
@@ -0,0 +1,39 @@
+﻿package ru.viscount.app;
+
+public class Calculator {
+    public void add(double number, double number1){
+       double sum = number + number1;
+
+    }
+    public void sub(double number, double number1){
+        double sum = number - number1;
+
+    }
+    public void mul(double number, double number1){
+        double sum = number * number1;
+
+    }
+
+//    public double div(double number, double number1){
+//
+//        if (number1 == 0){
+//            return  0;
+//            System.out.println("на ноль делить нельзя");
+//
+//        } else
+//        { double sum = number/number1;
+//
+//        }
+//
+//    }
+
+    public void mul(double number, double number1, double number2){
+        double sum = number * number1 * number2;
+
+    }
+    public void add(double number, double number1, double number2){
+        double sum = number + number1 + number2;
+
+    }
+
+}
---
+++ Tasks/src/main/java/ru/viscount/app/Employee.java
@@ -0,0 +1,26 @@
+﻿package ru.viscount.app;
+
+public class Employee {
+    String name;
+    String specialization;
+    int workExperience;
+
+    public Employee(){
+
+    }
+
+    public Employee(String n) {
+        name = n;
+    }
+
+    public Employee(String n, String s) {
+        name = n;
+        specialization = s;
+    }
+    public Employee(int w){
+        workExperience = w;
+    }
+
+
+
+}
