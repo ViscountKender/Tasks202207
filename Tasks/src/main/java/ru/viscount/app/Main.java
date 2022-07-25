57a9e84e28f2a8805a52260efed294717478503f
Viscount <siyanov.d@sar.garm.ru>
2022-07-26 00:26:47 +0400

    Merge remote-tracking branch 'origin/main'

--- Tasks/src/main/java/ru/viscount/app/Main.java
+++ Tasks/src/main/java/ru/viscount/app/Main.java
@@ -2,17 +2,29 @@ package ru.viscount.app;
 
 public class Main {
     public static void main(String[] args) {
+        double sumProfi = 0;
         Firma[] firma = {new Firma("Horns", 15,500),
                 new Firma("Hooves",25,900),
                 new Firma("Buy",8,1200),
                 new Firma("Sell",8,1600)};
-        double sum = 0;
-        for (int i = 0; i < firma.length; i++) {
-            sum = sum + firma[i].profit;
-            i++;
+
+        for (Firma value : firma) {
+
+            sumProfi = sumProfi + value.profit;
 
         }
-        System.out.println(sum);
+        System.out.println("Общая прибыль всех фирм: " + sumProfi);
+
+        double sunEmployees = 0;
+        for (Firma value : firma) {
+            sunEmployees = sunEmployees + value.numberOfEmployees;
+
+        }
+        System.out.println("Общее количество работников: " + sunEmployees);
+        double averageSunEmployees;
+        averageSunEmployees = sunEmployees/firma.length;
+        System.out.println("Среднее количество работников: " + averageSunEmployees);
+
 
 
 
