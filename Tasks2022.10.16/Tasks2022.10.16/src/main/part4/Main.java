2324372e964a709f9994f5c19d5f4fb955ad0428
Viscount <siyanov.d@sar.garm.ru>
2022-11-16 01:06:41 +0400

    that all

--- Tasks2022.10.16/Tasks2022.10.16/src/main/part4/Main.java
+++ Tasks2022.10.16/Tasks2022.10.16/src/main/part4/Main.java
@@ -2,11 +2,13 @@ package part4;
 
 public class Main {
     public static void main(String[] args) {
-        BankAccount bankAccount = new BankAccountIndividual();
-        BankAccount bankAccount1 = new LegalBankAccount();
-        bankAccount.refill(150);
-        bankAccount.withdraw(20);
-        bankAccount1.refill(50);
+
+        LegalBankAccount legalBankAccount = new LegalBankAccount();
+        BankAccountIndividual bankAccountIndividual = new BankAccountIndividual();
+        bankAccountIndividual.refill(150);
+        bankAccountIndividual.withdraw(20);
+        legalBankAccount.refill(50);
+        legalBankAccount.transfer(bankAccountIndividual, legalBankAccount,25);
 
 
 
